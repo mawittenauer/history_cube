3925e62b8907f41812468acba1a5d17457bcbd39
Mike Wittenauer <mikewittenauer@me.com>
2024-02-03 14:49:07 -0500

    Move word_wrapper to it's own hwlper class

--- index.rb
+++ index.rb
@@ -1,25 +1,7 @@
 require_relative "api"
+require_relative "helper"
 require "tty-prompt"
 require "terminal-table"
-
-def wrap_word(input_string, given_width = 40)
-  array_of_characters = input_string.split("")
-  output_string = []
-  counter_variable = 0
-  array_of_characters.each do |character|
-
-  if character == "\n"
-    counter_variable = 0
-
-  elsif counter_variable >= given_width && character == " "
-    character = "\n"
-    counter_variable = 0
-  end
-    output_string << character
-    counter_variable += 1
-  end
-  output_string.join("").to_s
-end
 
 class ConsoleApp
   def initialize(data)
@@ -45,7 +27,7 @@ class ConsoleApp
       system('clear') || system('cls')
       event = @data[index]
       rows = []
-      rows << ["#{event['month']}-#{event['day']}-#{event['year']}", "#{wrap_word(event['event'], 40)}"]
+      rows << ["#{event['month']}-#{event['day']}-#{event['year']}", "#{Helper.wrap_word(event['event'], 40)}"]
       table = Terminal::Table.new(:title => "History Facts", :headings => ['Date', 'Event'], :rows => rows)
       puts table
       sleep 10
@@ -65,4 +47,3 @@ end
 data = ["Roman Empire", "World War II", "Revolutionary War"]
 app = ConsoleApp.new(data)
 app.run
-
