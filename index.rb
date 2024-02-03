eeb4fa02930fa52b71405035eaa40a5175fb8223
Mike Wittenauer <mikewittenauer@me.com>
2024-02-03 07:38:43 -0500

    Upgrade line wrapping function

--- index.rb
+++ index.rb
@@ -2,8 +2,23 @@ require_relative "api"
 require "tty-prompt"
 require "terminal-table"
 
-def add_line_breaks(input_string, break_length)
-  input_string.scan(/.{1,#{break_length}}/).join("\n")
+def wrap_word(input_string, given_width = 40)
+  array_of_characters = input_string.split("")
+  output_string = []
+  counter_variable = 0
+  array_of_characters.each do |character|
+
+  if character == "\n"
+    counter_variable = 0
+
+  elsif counter_variable >= given_width && character == " "
+    character = "\n"
+    counter_variable = 0
+  end
+    output_string << character
+    counter_variable += 1
+  end
+  output_string.join("").to_s
 end
 
 class ConsoleApp
@@ -30,7 +45,7 @@ class ConsoleApp
       system('clear') || system('cls')
       event = @data[index]
       rows = []
-      rows << ["#{event['month']}-#{event['day']}-#{event['year']}", "#{add_line_breaks(event['event'], 40)}"]
+      rows << ["#{event['month']}-#{event['day']}-#{event['year']}", "#{wrap_word(event['event'], 40)}"]
       table = Terminal::Table.new(:title => "History Facts", :headings => ['Date', 'Event'], :rows => rows)
       puts table
       sleep 10
